0b89473b2c6574adc76a8037502f57b2a9611577
Dmitry <magicsugar25@gmail.com>
2023-09-13 00:05:37 +0200

    fixed paths

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const URL = "RHMZ.html";
+const URL = "RHMZ/RHMZ.html";
 
 const header = document.getElementById('h1')
 const table = document.getElementById('table')
@@ -18,9 +18,7 @@ const getQuotes = async () => {
                 const dates = doc.querySelector('h1').innerText;
                 const rezults = doc.querySelector('#test');
 
-console.log(dates)
-
-                header.appendChild(dates);
+                // header.appendChild(dates);
                 table.appendChild(rezults);
             });
         })
